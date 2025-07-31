1487f9efe246201b0ed3e861b04a9a3de02f26d5
shashankdaduria <146616661+shashankdaduria@users.noreply.github.com>
2025-07-31 12:23:33 +0530

    Create 4bitcomp.v

---
+++ LAB1/4bitcomp.v
@@ -0,0 +1,9 @@
+module 4biteqcomp(
+  input [3:0] A;
+  input [3:0] B;
+  output C;
+); //module instantiation
+ 
+  C = (A==B) ? 1 : 0; //assign 1 to C when equal(A and B)
+
+endmodule //end of module
