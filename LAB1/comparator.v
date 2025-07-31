807aab34b02acbe77c12476651b0933282025044
shashankdaduria <146616661+shashankdaduria@users.noreply.github.com>
2025-07-31 12:14:27 +0530

    Create comparator.v

---
+++ LAB1/comparator.v
@@ -0,0 +1,13 @@
+module (
+  input A;
+  input B;
+  output Y1;
+  output Y2;
+  output Y3;
+); 
+
+  Y1 = A & ~B; // when A is 1 and B is 0, i.e., A > B
+  Y2 = A ~^ B; // one if both A and B are same
+  Y3 = ~A & B; // when A is 0 and B is 1, i.e., A < B
+
+endmodule 
